57855179cb931c5cf3c17ed955590694877021ec
thien.dang <thien.dang@ncc.asia>
2021-07-15 15:33:12 +0700

    remove case for training

---
+++ cypress/tests/demo/auth.spec.ts
@@ -0,0 +1,10 @@
+describe("User Sign-up and Login", function () {
+  // su dung task de clean hoac chuan bi db truoc khi run test case
+  it("should redirect unauthenticated user to signin page", function () {
+    // define case
+  });
+
+  it("should redirect to the home page after login", function () {
+    //
+  });
+});
